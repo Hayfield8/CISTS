1a4eabb14f84d287eb931eb44d365b8804458797
Mathew Hayfield <54106320+Hayfield8@users.noreply.github.com>
2020-08-18 14:01:02 +0100

    reworked some GUI to match designs,added cfboss and cf classes

---
+++ src/cists/CustomFieldBoss.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cists;
+
+import java.util.List;
+
+/**
+ *
+ * @author m_hay
+ */
+public class CustomFieldBoss {
+    
+     /**
+     * List containing all CustomField objects.
+     */
+    public List<CustomField> customFieldList;
+}
