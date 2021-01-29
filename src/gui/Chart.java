87096e4b86fa09b03637bc9d60701620a3c1adae
Mathew Hayfield <54106320+Hayfield8@users.noreply.github.com>
2021-01-29 12:27:21 +0000

    added shart class in gui package.

---
+++ src/gui/Chart.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import org.jfree.chart.*;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+/**
+ * GUI class to create and display charts.
+ * 
+ * @author mathew hayfield
+ * @since 29-01-2021
+ * @version 1.0
+ * 
+ */
+public class Chart extends javax.swing.JFrame{
+    
+    
+    
+}
